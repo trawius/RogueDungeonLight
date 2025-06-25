f29bfa9913c3e78609f4282456c880c99a73c5a7
trawius <57256275+trawius@users.noreply.github.com>
2025-06-25 14:44:07 +0300

    fix player and camera bug

--- Assets/Scripts/Player/CameraFollow.cs
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -3,12 +3,17 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
-    public float smoothSpeed = 5f;
+    public float smoothSpeed = 0.125f;
 
     void LateUpdate()
     {
         if (target == null) return;
         Vector3 desiredPosition = new Vector3(target.position.x, target.position.y, -10f);
-        transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+
+        //transform.position = vector3.lerp(transform.position, desiredposition, smoothspeed * time.deltatime);
+
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
     }
 }
